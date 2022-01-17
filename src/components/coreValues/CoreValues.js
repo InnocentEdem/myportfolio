473ade56d8142e4b29a7e5c5419f353aeb1552d6
InnocentEdem <innocent.kuwornu@amalitech.org>
2022-01-17 11:14:08 +0000

    update core values

---
+++ src/components/coreValues/CoreValues.js
@@ -0,0 +1,167 @@
+import React from 'react'
+import './coreValues.css'
+import SvgIcon from '@mui/material/SvgIcon';
+import WebIcon from '@mui/icons-material/Web';
+import CheckCircleOutlinedIcon from '@mui/icons-material/CheckCircleOutlined';
+import ZoomOutIcon from '@mui/icons-material/ZoomOut';
+import PeopleAltOutlinedIcon from '@mui/icons-material/PeopleAltOutlined';
+
+export default function CoreValues() {
+    return (
+        <div>
+            <div className="core-values-container">
+                <div className="services-header">
+                    <div className="services-header-button">Services</div>
+                    <h2 className="services-header-text">
+                        My Core Values
+                    </h2>
+                    <p>
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit, 
+                    sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                    </p>
+                </div>
+                <div className="values-grid">
+                    <div className="value">
+                        <div className="core-value-icon">
+                            <div className="icon-circle">
+                            <SvgIcon style={{fontSize:"3rem",margin:"auto"}} ><PeopleAltOutlinedIcon/></SvgIcon>
+
+                            </div>
+                        </div>
+                        <div className="core-value-text">
+                        <div id='core-value' className="project">
+                           
+                            <div className="project-text">
+                                <h3 className="service-text-content-header">
+                                    Value
+                                </h3>
+                                <div className="service-texts-content-text">
+                                Lorem ipsum dolor sit amet, consectetur adipisicing elit,
+                                sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                                </div>
+
+                            </div>
+                        </div>
+                        </div>
+                    </div>
+                    <div className="value">
+                        <div className="core-value-icon">
+                            <div className="icon-circle">
+                            <SvgIcon style={{fontSize:"3rem",margin:"auto"}} ><PeopleAltOutlinedIcon/></SvgIcon>
+
+                            </div>
+                        </div>
+                        <div className="core-value-text">
+                        <div id='core-value' className="project">
+                           
+                            <div className="project-text">
+                                <h3 className="service-text-content-header">
+                                    Value
+                                </h3>
+                                <div className="service-texts-content-text">
+                                Lorem ipsum dolor sit amet, consectetur adipisicing elit,
+                                sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                                </div>
+
+                            </div>
+                        </div>
+                        </div>
+                    </div>
+                    <div className="value">
+                        <div className="core-value-icon">
+                            <div className="icon-circle">
+                            <SvgIcon style={{fontSize:"3rem",margin:"auto"}} ><PeopleAltOutlinedIcon/></SvgIcon>
+
+                            </div>
+                        </div>
+                        <div className="core-value-text">
+                        <div id='core-value' className="project">
+                           
+                            <div className="project-text">
+                                <h3 className="service-text-content-header">
+                                    Value
+                                </h3>
+                                <div className="service-texts-content-text">
+                                Lorem ipsum dolor sit amet, consectetur adipisicing elit,
+                                sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                                </div>
+
+                            </div>
+                        </div>
+                        </div>
+                    </div>
+                    <div className="value">
+                        <div className="core-value-icon">
+                            <div className="icon-circle">
+                            <SvgIcon style={{fontSize:"3rem",margin:"auto"}} ><PeopleAltOutlinedIcon/></SvgIcon>
+
+                            </div>
+                        </div>
+                        <div className="core-value-text">
+                        <div id='core-value' className="project">
+                           
+                            <div className="project-text">
+                                <h3 className="service-text-content-header">
+                                    Value
+                                </h3>
+                                <div className="service-texts-content-text">
+                                Lorem ipsum dolor sit amet, consectetur adipisicing elit,
+                                sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                                </div>
+
+                            </div>
+                        </div>
+                        </div>
+                    </div>
+                    <div className="value">
+                        <div className="core-value-icon">
+                            <div className="icon-circle">
+                            <SvgIcon style={{fontSize:"3rem",margin:"auto"}} ><PeopleAltOutlinedIcon/></SvgIcon>
+
+                            </div>
+                        </div>
+                        <div className="core-value-text">
+                        <div id='core-value' className="project">
+                           
+                            <div className="project-text">
+                                <h3 className="service-text-content-header">
+                                    Value
+                                </h3>
+                                <div className="service-texts-content-text">
+                                Lorem ipsum dolor sit amet, consectetur adipisicing elit,
+                                sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                                </div>
+
+                            </div>
+                        </div>
+                        </div>
+                    </div>
+                    <div className="value">
+                        <div className="core-value-icon">
+                            <div className="icon-circle">
+                            <SvgIcon style={{fontSize:"3rem",margin:"auto"}} ><PeopleAltOutlinedIcon/></SvgIcon>
+
+                            </div>
+                        </div>
+                        <div className="core-value-text">
+                        <div id='core-value' className="project">
+                            <div className="project-text">
+                                <h3 className="service-text-content-header">
+                                    Value
+                                </h3>
+                                <div className="service-texts-content-text">
+                                Lorem ipsum dolor sit amet, consectetur adipisicing elit,
+                                sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                                </div>
+
+                            </div>
+                        </div>
+                        </div>
+                    </div>
+                    
+                </div>
+            </div>
+            
+        </div>
+    )
+}
